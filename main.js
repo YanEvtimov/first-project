09ae580693c2a46b6f9af5a99015d6ef718757e1
Yan Evtimov <ysevtimov@gmail.com>
2019-03-05 17:23:38 +0200

    made changes to improve functionality

--- main.js
+++ main.js
@@ -2,16 +2,14 @@ $(document).ready(init);
 
 // initialize the app
 function init() {
-    // by default load the products
+    // by default load a sign in form
     $('#sec2-3').html($('#signInForm').html());
-    //initProducts();
-
 }
 
 function initForm() {
     $('#sec2-3 form').submit((e) => {
         e.preventDefault();
-        // get all form data - it looks like [{name: 'image' value 'http://...'}, {name: 'description', value: '...'}...]
+        // get all form data
         const frm = $('#sec2-3 form').serializeArray();
 
         let data = {}
@@ -24,3 +22,34 @@ function initForm() {
             .catch(() => alert('There was a problem adding the subscriber...'))
     })
 }
+
+function getSubscribers() {
+    $('#sec2-3 div p a').click((e) => {
+        e.preventDefault();
+
+        $('#sec2-3 article').style.visibility = "visible";
+    })
+}
+
+function closeArticle() {
+    $('#sec2-3 article').style.visibility = "hidden";
+}
+
+/*function initSubs() {
+    // get the product template as text
+    let tpl = $('#hiddenSubs').html(), tpl1;
+    // get the products
+    $.get('http://localhost:3000/subscribers')
+        .then((subs) => {
+            console.log(subs);
+            subs.forEach((sub) => {
+                // wrap it in div and create jQuery element of it
+                tpl1 = $('<div>' + tpl + '</div>')
+                tpl1.find('h3').html(sub.name);
+                tpl1.find('.desc').html(sub.name + ' description');
+                console.log(sub.name, tpl1.html())
+                $('main .product-list').append(tpl1);
+            })
+
+        })
+}*/
