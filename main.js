55d7642f6ac0326136bce04d82bef7c225671c54
Yan Evtimov <ysevtimov@gmail.com>
2019-03-07 17:37:24 +0200

    make changes for subscribers to appear only once

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ $(document).ready(init);
 function init() {
     // by default load a sign in form
     $('#sec2-3').html($('#signInForm').html());
+    //checkForMail();
 }
 
 function initForm() {
@@ -23,21 +24,53 @@ function initForm() {
     })
 }
 
+/*function checkForMail() {
+    $('#mail').blur((e) => {
+        e.preventDefault();
+
+        let mailInput = $('#mail').val();
+        let dbJson = '';
+
+        $.get('http://localhost:3000/subscribers')
+            .then((subs) => {
+                console.log(subs);
+                subs.forEach((sub) => {
+                    // wrap it in div and create jQuery element of it
+                    dbJson = (sub.Mail);
+                })
+        
+        if (mailInput === dbJson) {
+            alert('There already is a subscriber with this e-mail, please use other');
+        }
+    })
+}*/
+
 function getSubscribers() {
     $('#sec2-3 div p a').click((e) => {
         e.preventDefault();
 
-        $('#sec2-3 article').style.visibility = "visible";
+        //initSubs();
+        $('#sec2-3 article').css('visibility', 'visible');
+        
+    })
+
+    $('#sec2-3 div p a').one('click', (e) => {
+        e.preventDefault();
+
+        initSubs();
     })
 }
 
 function closeArticle() {
-    $('#sec2-3 article').style.visibility = "hidden";
+    $('#sec2-3 article section.clsBtn').click((e) => {
+        $('#sec2-3 article').css('visibility', 'hidden');
+    })
 }
 
-/*function initSubs() {
-    // get the product template as text
-    let tpl = $('#hiddenSubs').html(), tpl1;
+function initSubs() {
+    // get the subscriber template as text
+    $('#hiddenSubs p').html($('#allSubs').html())
+    let tpl = $('#subscriber').html(), tpl1;
     // get the products
     $.get('http://localhost:3000/subscribers')
         .then((subs) => {
@@ -45,11 +78,11 @@ function closeArticle() {
             subs.forEach((sub) => {
                 // wrap it in div and create jQuery element of it
                 tpl1 = $('<div>' + tpl + '</div>')
-                tpl1.find('h3').html(sub.name);
-                tpl1.find('.desc').html(sub.name + ' description');
+                tpl1.find('h3').html(sub.Name);
+                tpl1.find('.info').html(sub.Mail);
                 console.log(sub.name, tpl1.html())
-                $('main .product-list').append(tpl1);
+                $('.subs-list').append(tpl1);
             })
 
         })
-}*/
+}
